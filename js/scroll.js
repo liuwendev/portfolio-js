e4879a1b4b271ba3d3ef25d203e45cc0150d9b8b
Liu Wen <haba-haba555@ya.ru>
2022-03-21 03:02:59 +0300

    added timer, scroll js

---
+++ js/scroll.js
@@ -0,0 +1,23 @@
+const links = document.querySelectorAll('.menu-list__link');
+const btn = document.querySelector('.main__button');
+const scrollBtn = document.querySelector('.main__scroll');
+
+const allLinks = [...links, btn, scrollBtn];
+console.log(allLinks);
+
+allLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        
+        const id = link.getAttribute('href').substring(1);
+        const section = document.getElementById(id);
+        
+        if (section) {
+            seamless.scrollIntoView(section, {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+        }
+    })
+})
